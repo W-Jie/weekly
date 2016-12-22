eb1c03231056388bf846659445f86a97db070df4
W.Jie <fwjfengweijie@126.com>
2016-12-22 23:46:57 +0800

    格式化输出到文本文件

--- weekly.go
+++ weekly.go
@@ -1,94 +1,99 @@
 package main
 
 import (
-	_ "encoding/json"
+	"encoding/json"
 	"flag"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
-	_ "sort"
+	"sort"
 	"strings"
+	"time"
 )
 
-// type Report struct {
-// 	ProjectName []string
-// 	ProjectNo   []string
-// 	Name        []string
-// 	Description []string
-// 	Question    []string
-// 	Plan        []string
-// }
+type Report struct {
+	ProjectName string `json: "ProjectName"`
+	ProjectNo   string `json: "ProjectNo"`
+	Name        string `json: "Name"`
+	Description string `json: "Description"`
+	Issue       string `json: "Issue"`
+	Planning    string `json: "Planning"`
+	LogTime     string `json: "LogTime"`
+}
 
-var infile *string = flag.String("i", "", "输入文件")
-var outfile *string = flag.String("o", "", "输出文件")
+var infile *string = flag.String("i", "", "输入文件（必须）")
+var outfile *string = flag.String("o", "output.txt", "输出文件（可选）")
 
 func main() {
 	flag.Parse()
-
-	content, err := readAll(*infile)
-	if err != nil {
-		panic(err)
+	//fmt.Println(os.Args[0])
+	if len(os.Args) < 2 {
+		// log.Fatal("infile can not be blank!")
+		flag.PrintDefaults()
 	}
+	content := ReadAll(*infile)
+	rows := GetRows(&content)
+	rows = RmDuplicate(&rows)
 
-	rows := getRows(&content)
-	//fmt.Println(rows)
-	rows = rm_duplicate(&rows)
+	now := time.Now().Unix()
+	rlist := ReportList(make([]Report, 0))
 
-	lines := make([]string, 0)
 	for _, row := range rows {
-		lines = append(lines, format2txt(&row))
+		rpt := Format(&row, now)
+		//fmt.Println(tojson(&rpt))
+		rlist = append(rlist, rpt)
 	}
-	saveFile(*outfile, &lines)
+
+	sort.Sort(rlist) // 返回[]Report
+	//fmt.Println(rlist)
+	SaveFile(*outfile, &rlist)
 }
 
 // 读取文件内容
-func readAll(filepath string) (content string, err error) {
+func ReadAll(filepath string) (content string) {
 	file, err := os.Open(filepath)
 	if err != nil {
-		fmt.Println("Failed to open file: ", filepath)
-		return
+		log.Fatal(err)
 	}
 	defer file.Close()
 
 	ra, err := ioutil.ReadAll(file)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 	content = string(ra)
-	fmt.Printf("<<读取文件 %s 成功\n", filepath)
+	log.Printf("读取文件 <<%s 成功\n", filepath)
 	return
 }
 
 // 保存文件
-func saveFile(filepath string, contents *[]string) {
+func SaveFile(filepath string, contents *ReportList) bool {
 	//删除已存在文件
-	if _, err := os.Stat(filepath); err == nil {
-		err = os.Remove(filepath)
-		if err != nil {
-			fmt.Println("Failed to remove file: ", filepath)
-			panic(err)
-		}
-	}
+	// if _, err := os.Stat(filepath); err == nil {
+	// 	err = os.Remove(filepath)
+	// 	if err != nil {
+	// 		fmt.Println("Failed to remove file: ", filepath)
+	// 		return false
+	// 	}
+	// }
 
 	file, err := os.OpenFile(filepath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
 	if err != nil {
-		fmt.Println("Failed to open file:", filepath)
-		panic(err)
+		log.Fatal(err)
 	}
 	defer file.Close()
 
 	// 逐行写入
 	for _, content := range *contents {
-		file.WriteString(content)
-		file.WriteString("\n")
+		out := content.ProjectName + "\t" + content.ProjectNo + "\t" + content.Name + "\t" + content.Description + "\t" + content.Issue + "\t" + content.Planning + "\n"
+		file.WriteString(out)
 	}
-	fmt.Printf(">>保存文件 %s 成功\n", filepath)
-
+	log.Printf("保存文件 >>%s 成功\n", filepath)
+	return true
 }
 
-// 分割文本行
-func getRows(content *string) (rows []string) {
+// 分割行
+func GetRows(content *string) (rows []string) {
 	for _, row := range strings.Split(*content, "\n") {
 		//row = strings.Replace(row, "/", "", 1) //去除开头"/"符号
 		row = strings.TrimSpace(strings.Replace(row, "\n", "", -1)) //去除换行符&去除空格
@@ -100,7 +105,7 @@ func getRows(content *string) (rows []string) {
 }
 
 // 去重
-func rm_duplicate(list *[]string) (x []string) {
+func RmDuplicate(list *[]string) (x []string) {
 	for _, i := range *list {
 		if len(x) == 0 {
 			x = append(x, i)
@@ -118,8 +123,60 @@ func rm_duplicate(list *[]string) (x []string) {
 	return
 }
 
-// 格式化文本
-func format2txt(rows *string) (lines string) {
-	lines = strings.Replace(*rows, "/", "\t", -1)
+// 每行结构化
+func Format(rows *string, sendtime int64) Report {
+	lines := strings.Split(*rows, "/")
+	if len(lines) != 6 {
+		log.Fatal("格式异常: ", *rows)
+	} else if len(lines[2]) > 12 {
+		log.Fatal("姓名格式异常: ", *rows)
+	}
+
+	logtime := time.Unix(int64(sendtime), 0).Format("2006-01-02 15:04:05")
+	rpt := Report{
+		ProjectName: lines[0],
+		ProjectNo:   lines[1],
+		Name:        lines[2],
+		Description: lines[3],
+		Issue:       lines[4],
+		Planning:    lines[5],
+		LogTime:     logtime,
+	}
+	return rpt
+}
+
+// 结构体切片排序
+type ReportList []Report // ReportList为sort接口的类型，需要实现Less()、Swap()和Len()方法;
+
+func (list ReportList) Len() int {
+	return len(list)
+}
+
+func (list ReportList) Less(i, j int) bool {
+	if list[i].ProjectNo < list[j].ProjectNo {
+		return true
+	} else if list[i].ProjectNo > list[j].ProjectNo {
+		return false
+	} else if list[i].ProjectName < list[j].ProjectName {
+		return true
+	} else if list[i].ProjectName > list[j].ProjectName {
+		return false
+	} else {
+		return list[i].Name < list[j].Name
+	}
+}
+
+func (list ReportList) Swap(i, j int) {
+	list[i], list[j] = list[j], list[i]
+
+}
+
+// json串行化
+func Tojson(rpt *Report) (toj string) {
+	j, err := json.Marshal(&rpt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	toj = string(j)
 	return
 }
